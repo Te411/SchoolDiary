5dbb4dba31127530afcdbced985f5036c21b4f49
Danila Ikryanikov <danila.ikryanikov@mail.ru>
2025-04-01 23:21:25 +0300

    Create project

---
+++ MyWebApplicationServer/DTO/User/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace MyWebApplicationServer.DTO.User
+{
+    public class LoginRequest
+    {
+        /// <summary>
+        /// Логин
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ MyWebApplicationServer/Data/LibraryContex.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.MyWebApplicationServer.Models;
+using System.Diagnostics;
+
+namespace MyWebApplicationServer.Data
+{
+    /// <summary>
+    /// Класс, представляющий контекст для Entity Framework
+    /// </summary>
+    public class LibraryContext : DbContext
+    {
+        /// <summary>
+		/// Таблица - "пользователь"
+		/// </summary>
+		public DbSet<Users> Users { get; set; }
+
+        /// <summary>
+		/// Конфигурационный файл
+		/// </summary>
+		private readonly IConfiguration _configuration;
+
+        /// <summary>
+		/// Конструктор по умолчанию
+		/// </summary>
+		public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        /// <summary>
+		/// Настройки генерации моделей
+		/// </summary>
+		/// <param name="modelBuilder"></param>
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>()
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Users>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Users>()
+                .Property(u => u.Login)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Users>()
+                .HasIndex(u => u.Login)
+                .IsUnique();
+        }
+    }
+}
